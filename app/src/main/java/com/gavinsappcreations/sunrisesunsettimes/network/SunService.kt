fe510c027dd63d3382cff805e7ba6b6bd109cf77
Gavin Wright <gavingt@gmail.com>
2020-01-18 07:13:56 -0700

    Got rid of Retrofit Coroutine call adapter Factory as it's no longer needed with Retrofit 2.6

--- app/src/main/java/com/gavinsappcreations/sunrisesunsettimes/network/SunService.kt
+++ app/src/main/java/com/gavinsappcreations/sunrisesunsettimes/network/SunService.kt
@@ -1,9 +1,7 @@
 package com.gavinsappcreations.sunrisesunsettimes.network
 
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import kotlinx.coroutines.Deferred
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -38,7 +36,6 @@ object SunNetwork {
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://api.sunrise-sunset.org/")
         .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .build()
 
     val sunData = retrofit.create(SunService::class.java)
